145482cfc8f01a82b0d061f0005de787c4fbb572
Eron <eronra@gmail.com>
2017-09-19 16:30:05 -0300

    Adding pull requests route test

--- test/sire_api/ports/handler/routes_test.clj
+++ test/sire_api/ports/handler/routes_test.clj
@@ -3,6 +3,7 @@
   (:require
     [com.stuartsierra.component :as component]
     [io.pedestal.test :refer :all]
+    [cheshire.core :refer :all]
     [sire-api.pedestal-component-test-util :as cpt]
     [io.pedestal.http :as http]
     [sire-api.ports.handler.component-pedestal :as pedestal-component]
@@ -27,10 +28,21 @@
   (constantly (test-system env)))
 
 (facts "Application routes"
-  (fact "Home page must be return a message"
+
+  (fact "Route '/' must be return a message"
      (cpt/with-system #'system init-fn
-                      (let [response (response-for (cpt/service system) :get "/")
-                            {:keys [body status headers]} response]
-                        status => 200
-                        "text/plain" => (get headers "Content-Type")
-                        body => "Welcome to Sire API!"))))
+        (let [response (response-for (cpt/service system) :get "/")
+              {:keys [body status headers]} response]
+          status => 200
+          "text/plain" => (get headers "Content-Type")
+          body => "Welcome to Sire API!")))
+
+   (facts "Pull requests routes")
+
+     (fact "Route '/pull-requests' must be return a list of pull requests"
+       (cpt/with-system #'system init-fn
+          (let [response (response-for (cpt/service system) :get "/pull-requests")
+                {:keys [body status headers]} response
+                body-parsed (parse-string body true)]
+            status => 200
+            body-parsed => [{:id 1 :name "test"} {:id 2 :name "test2"}]))))
